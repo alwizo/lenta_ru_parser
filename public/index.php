f401e14c8bb28511a1dcd9bef7fdfd98c9c28687
alwizo <alwizo.com@gmail.com>
2017-04-22 14:14:11 +0200

    Added roots needed for our application

--- public/index.php
+++ public/index.php
@@ -27,5 +27,9 @@ $router = new Core\Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-    
+$router->add('show/{id:\d+}', ['controller' => 'Home', 'action' => 'show']);
+$router->add('parser/download', ['controller' => 'Parser', 'action' => 'parseNews']);
+$router->add('parser/daily_news', ['controller' => 'Parser', 'action' => 'dailyNewstoCsv']);
+$router->add('parser/test', ['controller' => 'Parser', 'action' => 'test']);
+
 $router->dispatch($_SERVER['QUERY_STRING']);
